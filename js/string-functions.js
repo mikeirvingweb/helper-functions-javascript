e90f7ad26103bd2a9891bae0690ac294b91a2d40
Mike Irving <79252299+mikeirvingweb@users.noreply.github.com>
2022-02-21 17:32:12 +0000

    Update string-functions.js

--- js/string-functions.js
+++ js/string-functions.js
@@ -1,3 +1,9 @@
+/* Check and Email Address string looks to be in the correct format. Usage ValidateEmail("user@domain.com"); returns boolean */
+function ValidateEmail(email) {
+	var patEmail = /^[-._&0-9a-zA-Z]+[@][-._&0-9a-zA-Z]+[.][._0-9a-zA-Z]+[a-zA-Z]$/;
+	return patEmail.exec(email) != null;
+}
+
 /* Return a HTML Encoded String. e.g. HTMLEncode("1 & 2"); returns '1 &amp; 2' */
 function HTMLEncode(inString) {
 	return $('<div/>').text(inString).html();
